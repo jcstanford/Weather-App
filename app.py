e0bd65bf5cb1924c3a2fe6b19b2cff79c905dadb
colin <colinstanford092@gmail.com>
2021-08-02 13:20:58 -0600

    add city database

--- app.py
+++ app.py
@@ -6,28 +6,61 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+db = SQLAlchemy(app)
+
+class City(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def index():
+	cities = City.query.all()
+
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=cc605205b496f4d5334d53adc06d8432'
 	city = 'Las Vegas'
 
+	weather_data = [ ]
+
+	for city in cities:
 
 
-	r = requests.get(url.format(city)).json()
+		r = requests.get(url.format(city)).json()
 
-	weather = {
-		'city' : city,
-		'min-temp' : r['main']['temp_min'],
-		'max-temp' : r['main']['temp_max'],
-		'temperature' : r['main']['temp'],
-		'humidity' : r['main']['humidity'],
-		'description' : r['weather'][0]['description'],
-		'icon' : r['weather'][0]['icon']
-	}
+		weather = {
+			'city' : city,
+			'temperature' : r['main']['temp'],
+			'humidity' : r['main']['humidity'],
+			'description' : r['weather'][0]['description'],
+			'icon' : r['weather'][0]['icon']
+		}
+
+		weather_data.append(weather)
+
+
 
 	print(weather)
 
-	return render_template('index.html', weather=weather)
+	return render_template('index.html', weather_data=weather_data)
 
-# main, description, icon, min/max temp, humidity
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
